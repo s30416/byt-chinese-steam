9d99d5df4bc448774740256a3b23d3f8cb08023e
s30920 <s30920@pjwstk.edu.pl>
2025-11-13 03:56:46 +0200

    Created User class, tests for it, and class for attribute with validation :)

---
+++ src/BytChineseSteam.Models/User.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BytChineseSteam.Models;
+
+public abstract class User
+{
+ 
+    [Required]
+    public Name Name { get; set; } = null!;
+    
+    [Required]
+    [ValidEmail]
+    public string Email { get; set; } = null!;
+
+    [RegularExpression(@"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$")]
+    public string PhoneNumber { get; set; } = null!;
+    
+    
+    public string HashedPassword { get; set; } = null!;
+
+
+    protected User(Name name, string email, string phoneNumber, string hashedPassword)
+    {
+        Name = name;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        HashedPassword = hashedPassword;
+    }
+    
+    public User() {}
+    
+}
+
+
+public class Name
+{
+    [Required]
+    public string FirstName { get; set; } = null!;
+    
+    
+    [Required]
+    public string LastName { get; set; } = null!;
+
+    public Name(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+    
+    public Name() {}
+    
+}
