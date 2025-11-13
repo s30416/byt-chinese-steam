b1aa37c9132e6e8cf70963ea6e58a28c72ebc0ee
Andrii Sysoiev <sisoev2006a@gmail.com>
2025-11-13 13:24:52 +0100

    updated ExtentService to facilitate new classes

--- src/BytChineseSteam.Repository/Utils/ExtentService.cs
+++ src/BytChineseSteam.Repository/Utils/ExtentService.cs
@@ -9,10 +9,14 @@ namespace BytChineseSteam.Repository.Utils
         // placeholder for now — later replace with config value from appsettings.json
         private static readonly string BasePath = "C:\\Users\\Andrii Sysoiev\\RiderProjects\\byt-chinese-steam\\src\\BytChineseSteam.Repository\\Storage";
 
-        private static readonly string EmployeesPath = Path.Combine(BasePath, "employees.json");
         private static readonly string LimitedKeysPath = Path.Combine(BasePath, "limited_keys.json");
+        private static readonly string KeysPath = Path.Combine(BasePath, "keys.json");
         private static readonly string PublishersPath = Path.Combine(BasePath, "publishers.json");
         private static readonly string UsersPath = Path.Combine(BasePath, "users.json");
+        private static readonly string EmployeesPath = Path.Combine(BasePath, "employees.json");
+        private static readonly string SuperAdminsPath = Path.Combine(BasePath, "super_admins.json");
+        private static readonly string ManagersPath = Path.Combine(BasePath, "managers.json");
+        private static readonly string AdminsPath = Path.Combine(BasePath, "admins.json");
 
         private static readonly JsonSerializerOptions Options = new()
         {
@@ -57,14 +61,29 @@ namespace BytChineseSteam.Repository.Utils
 
         private static string GetPathForType(Type type)
         {
-            if (type == typeof(List<Employee>))
-                return EmployeesPath;
             if (type == typeof(List<Publisher>))
                 return PublishersPath;
+            
+            if (type == typeof(List<Key>))
+                return KeysPath;
+            
             if (type == typeof(List<Limited>))
                 return LimitedKeysPath;
+            
             if (type == typeof(List<User>))
                 return UsersPath;
+            
+            if (type == typeof(List<Employee>))
+                return EmployeesPath;
+            
+            if (type == typeof(List<Admin>))
+                return AdminsPath;
+            
+            if (type == typeof(List<SuperAdmin>))
+                return SuperAdminsPath;
+            
+            if (type == typeof(List<Manager>))
+                return ManagersPath;
 
             throw new ArgumentException($"No path mapping defined for type {type.Name}");
         }
