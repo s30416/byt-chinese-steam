206520bda82ab01064408adee7273eb63c09cb5f
Andrii Sysoiev <sisoev2006a@gmail.com>
2025-11-13 03:31:56 +0100

    Employee.cs basis. to be continued

---
+++ src/BytChineseSteam.Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace BytChineseSteam.Models;
+
+public abstract class Employee // extends User class in the future
+{
+    
+    public decimal? Salary { get; set; }
+
+    public decimal CollectedBonuses { get; set; } = 0;
+
+    protected Employee(decimal? salary, decimal collectedBonuses)
+    {
+        Salary = salary;
+        CollectedBonuses = collectedBonuses;
+    }
+
+    protected Employee(decimal? salary)
+    {
+        Salary = salary;
+    }
+
+    protected Employee() {}
+}
