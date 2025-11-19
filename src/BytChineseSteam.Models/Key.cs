12c5053540494562fcbf2341dde20eddcbb44744
thebocher <qurka.kolika@gmail.com>
2025-11-19 13:03:59 +0100

    moved extent to repository

--- src/BytChineseSteam.Models/Key.cs
+++ src/BytChineseSteam.Models/Key.cs
@@ -1,11 +1,17 @@
 using System.ComponentModel.DataAnnotations;
 using BytChineseSteam.Models.DataAnnotations;
+using BytChineseSteam.Repository.Extent;
 
 namespace BytChineseSteam.Models;
 
 public class Key : Limited
 {
     public static readonly Extent<Key> Extent = new ();
+
+    public Key()
+    {
+        Extent.Add(this);
+    }
     
     [MinLength(1)]
     public required string AccessKey { get; set; }
--- src/BytChineseSteam.Repository/Extent/ExtentPersistence.cs
+++ src/BytChineseSteam.Repository/Extent/ExtentPersistence.cs
@@ -1,17 +1,7 @@
-using System.Collections.ObjectModel;
-using System.Data;
 using System.Reflection;
-using System.Text.Json;
 using System.Text.Json.Nodes;
 
-namespace BytChineseSteam.Models;
-
-public interface IExtent
-{
-    string Name => throw new NotImplementedException();
-    void LoadFromJsonArray(JsonArray element);
-    JsonNode ToJson();
-}
+namespace BytChineseSteam.Repository.Extent;
 
 public abstract class ExtentPersistence
 {
@@ -82,54 +72,10 @@ public abstract class ExtentPersistence
             var classNode = data[extent.Name];
 
             if (classNode is JsonArray array)
+            {
+                // create one by one through validation
                 extent.LoadFromJsonArray(array);
+            }
         }
-    }
-}
-
-public class Extent<T> : IExtent
-{
-    private List<T> _items = [];
-    public string Name => typeof(T).Name;
-
-    public Extent()
-    {
-        ExtentPersistence.Register(this);
-    }
-
-    public void Add(T item)
-    {
-        if (item == null)
-        {
-            throw new ArgumentException("Item cannot be null");
-        }
-
-        // can call validation here
-
-        _items.Add(item);
-    }
-
-    public ReadOnlyCollection<T> All()
-    {
-        return _items.AsReadOnly();
-    }
-
-    public void LoadFromJsonArray(JsonArray element)
-    {
-        try
-        {
-            var items = element.Deserialize<List<T>>();
-            _items = items ?? throw new ArgumentException($"Received null for {Name} items");
-        }
-        catch (Exception e)
-        {
-            if (e is JsonException je || e is NotSupportedException ne)
-                throw new Exception($"Error loading items for {Name}. Invalid value in JSON");
-        }
-    }
-
-    public JsonNode ToJson()
-    {
-        return JsonSerializer.SerializeToNode(All())!;
     }
 }
