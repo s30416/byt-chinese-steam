fa43af1e84165ed11250263915afee3b647e7d99
s30920 <s30920@pjwstk.edu.pl>
2025-12-10 02:54:46 +0200

    CompositionTests are fixed, working on OrderKeyAssTests

--- src/BytChineseSteam.Tests/CompositionTests.cs
+++ src/BytChineseSteam.Tests/CompositionTests.cs
@@ -21,8 +21,8 @@ namespace BytChineseSteam.Tests
             var publisher = new Publisher("Ubisoft", "Global Publisher", admin);
             var game = new Game("Far Cry 6", "Shooter", publisher, admin);
 
-            var key = new Key(game, "123-ABC", 59.99m, DateTime.Now, 0, new List<string> { "Base Game" });
-
+            var key = new Key(game, admin, "123-ABC", 59.99m, DateTime.Now, 0, new List<string> { "Base Game" });
+            
             Assert.That(key.Game, Is.EqualTo(game));
             Assert.That(game.Keys, Does.Contain(key));
             Assert.That(game.Keys.Count, Is.EqualTo(1));
@@ -31,10 +31,12 @@ namespace BytChineseSteam.Tests
         [Test]
         public void CreateKey_WithNullGame_ShouldThrowException()
         {
-            var ex = Assert.Throws<ArgumentNullException>(() => 
-                new Key(null!, "123-ABC", 59.99m, DateTime.Now, 0, new List<string> { "Base Game" }));
+            var admin = new Admin(new Name("Big", "Tommy"), "big.tommy@example.com", "+48123456789", "howdoesourhashedpasswork", null);
             
-            Assert.That(ex!.Message, Does.Contain("Key cannot exist without a Game"));
+            var ex = Assert.Throws<ArgumentNullException>(() => 
+                new Key(null!, admin, "123-ABC", 59.99m, DateTime.Now, 0, new List<string> { "Base Game" }));
+            
+            Assert.That(ex!.ParamName, Is.EqualTo("game"));
         }
 
         [Test]
@@ -43,9 +45,9 @@ namespace BytChineseSteam.Tests
             var admin = new Admin(new Name("Big", "Tommy"), "big.tommy@example.com", "+48123456789", "howdoesourhashedpasswork", null);
             var publisher = new Publisher("EA", "Sports Publisher", admin);
             var game = new Game("FIFA 24", "Sports", publisher, admin);
-            var key1 = new Key(game, "KEY-1", 10, DateTime.Now, 0, new List<string>{"A"});
-            var key2 = new Key(game, "KEY-2", 20, DateTime.Now, 0, new List<string>{"B"});
-
+            var key1 = new Key(game, admin, "KEY-1", 10, DateTime.Now, 0, new List<string>{"A"});
+            var key2 = new Key(game, admin, "KEY-2", 20, DateTime.Now, 0, new List<string>{"B"});
+            
             Assert.That(Key.Extent.All().Count, Is.EqualTo(2));
             Assert.That(Game.ViewAllGames.Count, Is.EqualTo(1));
 
@@ -63,8 +65,8 @@ namespace BytChineseSteam.Tests
             var admin = new Admin(new Name("Big", "Tommy"), "big.tommy@example.com", "+48123456789", "howdoesourhashedpasswork", null);
             var publisher = new Publisher("Valve", "PC Platform", admin);
             var game = new Game("Portal 2", "Puzzle", publisher, admin);
-            var key = new Key(game, "P2-KEY", 10, DateTime.Now, 0, new List<string>{"A"});
-
+            var key = new Key(game, admin, "P2-KEY", 10, DateTime.Now, 0, new List<string>{"A"});
+            
             key.DeleteKey();
 
             Assert.That(Key.Extent.All(), Does.Not.Contain(key));
