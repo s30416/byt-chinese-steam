1648e599710a5484e18f052f3f778feb2ae3c911
thebocher <qurka.kolika@gmail.com>
2026-01-08 16:34:16 +0100

    works now

--- src/BytChineseSteam.Models/Interfaces/IAdmin.cs
+++ src/BytChineseSteam.Models/Interfaces/IAdmin.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Immutable;
+using System.Text.Json.Serialization;
 
 namespace BytChineseSteam.Models.Interfaces;
 
@@ -7,6 +8,8 @@ public interface IAdmin
     
     IReadOnlyCollection<Game> Games { get; }
     ImmutableHashSet<Publisher> Publishers { get; }
+    
+    [JsonIgnore]
     IReadOnlyCollection<Key> CreatedKeys { get; }
     void AddGame(Game game);
     void RemoveGame(Game game);
--- src/BytChineseSteam.Tests/SuperAdminEmployeeAssociationTests.cs
+++ src/BytChineseSteam.Tests/SuperAdminEmployeeAssociationTests.cs
@@ -87,7 +87,7 @@ public class SuperAdminEmployeeAssociationTests
         var otherEmp = new Employee(new Name("Other", "Guy"), "random@b.com", "+48123456789", "passhjsdfsdhjf", 0, null);
         var otherSuperAdminRole = otherEmp.AssignSuperAdminRole();
         
-        var employee = Employee.CreateEmployee<Admin>("A", "B", "test@b.com", "+48123456789", "passdhsfvsdjhf", 0, _superAdminRole);
+        var employee = Employee.CreateEmployee<IAdmin>("A", "B", "test@b.com", "+48123456789", "passdhsfvsdjhf", 0, _superAdminRole);
 
         Assert.Throws<InvalidOperationException>(() => 
             otherSuperAdminRole.AddCreatedEmployee(employee)
